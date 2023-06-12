f88b63aa46cc1c46468696ce267b6d7f68a84561
ahsansajid25324 <sajidahsan67@gmail.com>
2023-06-12 19:06:04 +0500

    ahsan 4th commit

--- myproject/myapp/views.py
+++ myproject/myapp/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 
 def home(request):
-    return render(request,'homepage_3.html')
+    return render(request,'index.html')
 
 # Create your views here.
+def addBug(request):
+    return render(request,'addBug.html')
