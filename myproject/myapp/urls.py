0d1651db01b5c3afb7b90efd736dccb40f4ad3a8
Muhammad-Shaheer-khan <shaheerk2233@gmail.com>
2023-06-16 17:44:00 +0500

    delete icon done

--- myproject/myapp/urls.py
+++ myproject/myapp/urls.py
@@ -2,9 +2,10 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('', home,name='home'),
-    # path('home', home,name='home'),
-    path('addBug',addBug, name='addBug'),
-    path('record',record,name='record'),
-    path("saveform",saveform,name='saveform')
+    path('', home, name='home'),
+    path('addBug', addBug, name='addBug'),
+    path('record', record, name='record'),
+    path('record', record, name='record'),
+    path('delete/<str:record_id>/', delete_record, name='delete_record'),
+    path('saveform', saveform, name='saveform'),
 ]
--- myproject/myapp/views.py
+++ myproject/myapp/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
 from django.views.decorators.csrf import csrf_exempt
+from django.shortcuts import render, redirect
+from django.views.decorators.http import require_http_methods
+import uuid
 
 import sqlite3
 import random
@@ -19,6 +22,30 @@ def home(request):
 def addBug(request):
     return render(request,'addBug.html')
 
+@require_http_methods(['POST', 'GET'])
+def delete_record(request, record_id):
+    if request.method == 'POST':
+        with sqlite3.connect(BASE_DIR / 'data.db') as db:
+            cursor = db.cursor()
+            query = "DELETE FROM Form WHERE ReporterID = ?"
+            cursor.execute(query, (record_id,))
+
+        return redirect('record')
+
+    else:
+        with sqlite3.connect(BASE_DIR / 'data.db') as db:
+            cursor = db.cursor()
+            query = "SELECT ReporterID, ReporterName, BugType, Reason, SiteName, SiteLink, OwnerEmail FROM Form"
+            cursor.execute(query)
+            row = cursor.fetchall()
+            column_names = [description[0] for description in cursor.description]
+
+        record = []
+        for row in row:
+            r = dict(zip(column_names, row))
+            record.append(r)
+
+        return render(request, 'record.html', {'record': record})
 
 def record(request):
     with sqlite3.connect(BASE_DIR / 'data.db') as db:
@@ -40,22 +67,32 @@ def record(request):
 
     
     return render(request,'record.html',{'record':record})
-
 @csrf_exempt
 def saveform(request):
-    if request.method=='POST':
-        # bugID=request.POST.get('bugNumber')
-        reporterName=request.POST.get('reporterName')
-        bugType=request.POST.get('bugType')
-        reason=request.POST.get('bugReason')
-        siteName=request.POST.get('siteName')
-        siteLink=request.POST.get('siteLink')
-        ownerEmail=request.POST.get('ownerEmail')
-        with sqlite3.connect(BASE_DIR/'data.db') as db:
-            cursor=db.cursor()
-            query = "INSERT INTO Form (ReporterID,ReporterName,BugType,Reason,SiteName,SiteLink,OwnerEmail) VALUES (?, ?,?,?,?,?,?)"
-            values = ("".join(random.choices(string.digits, k=4)),reporterName,bugType,reason,siteName,siteLink,ownerEmail)
+    if request.method == 'POST':
+        reporterName = request.POST.get('reporterName')
+        bugType = request.POST.get('bugType')
+        reason = request.POST.get('bugReason')
+        siteName = request.POST.get('siteName')
+        siteLink = request.POST.get('siteLink')
+        ownerEmail = request.POST.get('ownerEmail')
+
+        with sqlite3.connect(BASE_DIR / 'data.db') as db:
+            cursor = db.cursor()
+
+            # Get the last inserted reporter ID from the database
+            query_last_id = "SELECT ReporterID FROM Form ORDER BY ReporterID DESC LIMIT 1"
+            cursor.execute(query_last_id)
+            result = cursor.fetchone()
+
+            if result is not None:
+                last_id = int(result[0])
+                new_id = str(last_id + 1).zfill(4)  # Increment the last ID and pad with leading zeros
+            else:
+                new_id = "0001"  # If there are no existing records, start with 0001
+
+            query = "INSERT INTO Form (ReporterID, ReporterName, BugType, Reason, SiteName, SiteLink, OwnerEmail) VALUES (?, ?, ?, ?, ?, ?, ?)"
+            values = (new_id, reporterName, bugType, reason, siteName, siteLink, ownerEmail)
             cursor.execute(query, values)
-            
-        
-    return render(request,'addBug.html')
+
+    return render(request, 'addBug.html')
